98a46e26789ea6586684e05126446157eae6d243
Alexandr Popov <cahek2317@mail.ru>
2020-10-20 21:58:48 +0300

    chore: add material types

---
+++ flow-typed/npm/@material-ui.js
@@ -0,0 +1,42 @@
+declare module '@material-ui/core' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/Paper' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/Grid' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/CssBaseline' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/Typography' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/Divider' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/Box' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/Tabs' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/core/Tab' {
+  declare module.exports: any
+}
+
+declare module '@material-ui/styles' {
+  declare module.exports: any
+}
+declare module '@material-ui/styles/makeStyles' {
+  declare module.exports: any
+}
